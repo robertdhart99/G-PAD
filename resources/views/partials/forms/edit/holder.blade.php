76b76ddd622c8b077d5cde3fe6b61050c8e0a98a
nguye4te <nguye4te@mail.uc.edu>
2022-03-26 16:24:41 -0400

    Rearranged Items in Assets

--- resources/views/partials/forms/edit/holder.blade.php
+++ resources/views/partials/forms/edit/holder.blade.php
@@ -3,9 +3,9 @@
 <!-- {{ old('holder', ($item->holder) ? $item->holder->format('Y-m-d') : '') }}-->
 
 <div class="form-group{{ $errors->has('holder') ? ' has-error' : '' }}">
-    <label for="holder" class="col-md-3 control-l abel">{{ trans('CheckBoxes') }}</label>
+    <label for="holder" class="col-md-3 control-label">{{ trans('CheckBoxes') }}</label>
     <div class="input-group col-md-3">
-        <div class="input-String" data-provide="string" data-string-format="EEEE"  data-autoclose="true">-->
+        <div class="input-String" data-provide="string" data-string-format="EEEE"  data-autoclose="true">
             
             <input type="checkbox" id="val1" name="val1" value="val1"> <!--strings-->
             <label for ="val1"> Value 1</label><br>
@@ -14,7 +14,7 @@
             <input type="checkbox" id="val3" name="val3" value="val3">
             <label for ="val3"> Value 3</label><br>
             <input type="checkbox" class="form-control" placeholder="{{ trans('general.string') }}" name="holder" id="va1" value="aa">
-            <span class="input-group-addon"><i class="fa fa-calendar" aria-hidden="true"></i></span>-->
+            <!--<span class="input-group-addon"><i class="fa fa-calendar" aria-hidden="true"></i></span>-->
 
         </div>
         {!! $errors->first('Holder', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
